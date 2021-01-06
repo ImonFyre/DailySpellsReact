675954ed2c613d8a9547a305ffb04f106ab677b1
Kyle <imonfyre@gmail.com>
2021-01-06 18:35:02 -0500

    Removed commented code

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import './App.css';
-import { ICharacterProp, ICharactersProp } from './components/interfaces/customInputProps';
+import { ICharactersProp } from './components/interfaces/customInputProps';
 
 import {BrowserRouter as Router, Route, useLocation} from "react-router-dom"
 import { NavItem } from './components/NavItem';
 import { Character } from './contents/Character';
-import { queryAllByAltText } from '@testing-library/react';
 
 
 
 class App extends React.Component<ICharactersProp>
 {
-
 	render()
 	{
-		/*let useQuery = () =>  new URLSearchParams(useLocation().search;
-		let query = useQuery();*/
 		let nav = this.props.characters.map((c,idx) => {
 			return <NavItem tolink={`/character/${idx}`} item={c.name} />
 		})
-		//<NavItem tolink='/character' item={this.props.characters[0].name}></NavItem>
 		return (
 			<main>
 				<Router>
@@ -35,8 +30,5 @@ class App extends React.Component<ICharactersProp>
 		);
 	}
 }
-
-
-
 
 export default App;
